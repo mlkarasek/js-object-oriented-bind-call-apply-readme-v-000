7ae1c3a1a102dcd6a54440edee46fffecfdf5395
Monica  Karasek <mlkarasek@gmail.com>
2018-09-14 20:53:30 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// use this to copy code snippets or us your browser's console
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+    console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally);
+
+newGreet('Bob');
+// Hi Bob, my name is Sally!
+
+greet('Bob');
+// Hi Bob, my name is !
